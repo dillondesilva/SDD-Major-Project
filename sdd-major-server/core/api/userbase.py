dfcaf3395eb3ab55a65b076a58ea4edb5f4fba7d
Dillon de Silva <dillon.desilva@geeksdiary.com>
2021-02-24 23:48:24 +1100

    Server creates new instance of stub user in MongoDB

--- sdd-major-server/core/api/userbase.py
+++ sdd-major-server/core/api/userbase.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, jsonify
+from pymongo import MongoClient
 
 userbase_api = Blueprint('userbase_api', __name__)
 
+db = MongoClient("mongodb://localhost:27017/?retryWrites=true&w=majority")
+
+# Test API to MongoDB
 @userbase_api.route("/create_user", methods=["POST"])
 def accountList():
+    db.userbase.users.insert_one({
+        "name": "test"
+    })
     response = jsonify("user added")
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
